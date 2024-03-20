574868ed291a72d545618a6699c7e3e739645cb7
Khodatskaa <khodatskaalina@gmail.com>
2024-03-20 19:15:52 +0200

    Ex 4 was done

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -2,8 +2,23 @@
 {
     internal class Program
     {
-        static void Main()
+        public static void Main()
         {
+            Func<double, double, double> addDelegate = ArithmeticOperations.Add;
+            Func<double, double, double> subtractDelegate = ArithmeticOperations.Subtract;
+            Func<double, double, double> multiplyDelegate = ArithmeticOperations.Multiply;
+
+            double operand1 = 10;
+            double operand2 = 5;
+
+            double additionResult = addDelegate.Invoke(operand1, operand2);
+            Console.WriteLine($"Addition: {operand1} + {operand2} = {additionResult}");
+
+            double subtractionResult = subtractDelegate.Invoke(operand1, operand2);
+            Console.WriteLine($"Subtraction: {operand1} - {operand2} = {subtractionResult}");
+
+            double multiplicationResult = multiplyDelegate.Invoke(operand1, operand2);
+            Console.WriteLine($"Multiplication: {operand1} * {operand2} = {multiplicationResult}");
         }
     }
 }
