fab0622da169d7ebf6947b2073f71e0d67d059b0
Khodatskaa <khodatskaalina@gmail.com>
2024-03-20 19:48:36 +0200

    Ex 2 was done

---
+++ Delegates/MethodSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class MethodSet
+    {
+        public static void DisplayCurrentTime()
+        {
+            Console.WriteLine("Current Time: " + DateTime.Now.ToShortTimeString());
+        }
+
+        public static void DisplayCurrentDate()
+        {
+            Console.WriteLine("Current Date: " + DateTime.Now.ToShortDateString());
+        }
+
+        public static void DisplayCurrentDayOfWeek()
+        {
+            Console.WriteLine("Current Day of the Week: " + DateTime.Now.DayOfWeek);
+        }
+
+        public static double CalculateTriangleArea(double baseLength, double height)
+        {
+            return 0.5 * baseLength * height;
+        }
+
+        public static double CalculateRectangleArea(double length, double width)
+        {
+            return length * width;
+        }
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,6 +4,22 @@
     {
         static void Main()
         {
+            Action displayTime = MethodSet.DisplayCurrentTime;
+            Action displayDate = MethodSet.DisplayCurrentDate;
+            Action displayDayOfWeek = MethodSet.DisplayCurrentDayOfWeek;
+
+            Func<double, double, double> calculateTriangleArea = MethodSet.CalculateTriangleArea;
+            Func<double, double, double> calculateRectangleArea = MethodSet.CalculateRectangleArea;
+
+            displayTime();
+            displayDate();
+            displayDayOfWeek();
+
+            double triangleArea = calculateTriangleArea(5, 8);
+            double rectangleArea = calculateRectangleArea(4, 12);
+
+            Console.WriteLine("Area of Triangle: " + triangleArea);
+            Console.WriteLine("Area of Rectangle: " + rectangleArea);
         }
     }
 }
