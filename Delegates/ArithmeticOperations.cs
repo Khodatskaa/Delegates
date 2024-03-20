3219e122e2a69982e7f89c75eaea6939f5253569
Khodatskaa <khodatskaalina@gmail.com>
2024-03-20 19:06:37 +0200

    Ex 3 was done

---
+++ Delegates/ArithmeticOperations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class ArithmeticOperations
+    {
+        // check if a number is even
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        // Predicate to check if a number is odd
+        public static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        // check if a number is prime
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+            if (number <= 3)
+                return true;
+
+            if (number % 2 == 0 || number % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= number; i += 6)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        // check if a number is a Fibonacci number
+        public static bool IsFibonacci(int number)
+        {
+            int a = 0, b = 1;
+            while (b < number)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return b == number;
+        }
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,6 +4,20 @@
     {
         static void Main()
         {
+            int number1 = 10;
+            int number2 = 13;
+            int number3 = 17;
+            int number4 = 21;
+
+            Predicate<int> evenPredicate = new Predicate<int>(ArithmeticOperations.IsEven);
+            Predicate<int> oddPredicate = new Predicate<int>(ArithmeticOperations.IsOdd);
+            Predicate<int> primePredicate = new Predicate<int>(ArithmeticOperations.IsPrime);
+            Predicate<int> fibonacciPredicate = new Predicate<int>(ArithmeticOperations.IsFibonacci);
+
+            Console.WriteLine($"Number {number1} is even: {evenPredicate(number1)}");
+            Console.WriteLine($"Number {number2} is odd: {oddPredicate(number2)}");
+            Console.WriteLine($"Number {number3} is prime: {primePredicate(number3)}");
+            Console.WriteLine($"Number {number4} is a Fibonacci number: {fibonacciPredicate(number4)}");
         }
     }
 }
