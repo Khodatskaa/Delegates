11d55055f5905f8ad6c82140e5a0893816111e23
Khodatskaa <khodatskaalina@gmail.com>
2024-03-20 19:00:38 +0200

    Ex 2 was done

---
+++ Delegates/ArithmeticOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate double ArithmeticOperation(double a, double b);
+
+    public class ArithmeticOperations
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,6 +4,16 @@
     {
         static void Main()
         {
+            ArithmeticOperation addDelegate = new ArithmeticOperation(ArithmeticOperations.Add);
+            ArithmeticOperation subtractDelegate = new ArithmeticOperation(ArithmeticOperations.Subtract);
+            ArithmeticOperation multiplyDelegate = new ArithmeticOperation(ArithmeticOperations.Multiply);
+
+            double operand1 = 10;
+            double operand2 = 5;
+
+            Console.WriteLine($"Addition: {operand1} + {operand2} = {addDelegate(operand1, operand2)}");
+            Console.WriteLine($"Subtraction: {operand1} - {operand2} = {subtractDelegate(operand1, operand2)}");
+            Console.WriteLine($"Multiplication: {operand1} * {operand2} = {multiplyDelegate(operand1, operand2)}");
         }
     }
 }
