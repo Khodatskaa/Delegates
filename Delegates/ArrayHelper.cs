23aa63bee037f13b666054acb33eba246b34a728
Khodatskaa <khodatskaalina@gmail.com>
2024-03-20 19:44:31 +0200

    Ex 1 was done

---
+++ Delegates/ArrayHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate bool FilterDelegate(int num);
+
+    public class ArrayHelper
+    {
+        public static List<int> FilterArray(int[] arr, FilterDelegate filter)
+        {
+            List<int> result = new List<int>();
+            foreach (int num in arr)
+            {
+                if (filter(num))
+                {
+                    result.Add(num);
+                }
+            }
+            return result;
+        }
+
+        public static bool IsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+
+        public static bool IsOdd(int num)
+        {
+            return num % 2 != 0;
+        }
+
+        public static bool IsPrime(int num)
+        {
+            if (num <= 1)
+                return false;
+            if (num == 2)
+                return true;
+            if (num % 2 == 0)
+                return false;
+
+            for (int i = 3; i <= Math.Sqrt(num); i += 2)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public static List<int> GetFibonacciNumbers(int count)
+        {
+            List<int> fibNumbers = new List<int>();
+            int a = 0, b = 1;
+            fibNumbers.Add(a);
+            fibNumbers.Add(b);
+            for (int i = 2; i < count; i++)
+            {
+                int c = a + b;
+                fibNumbers.Add(c);
+                a = b;
+                b = c;
+            }
+            return fibNumbers;
+        }
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,6 +4,39 @@
     {
         static void Main()
         {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("Even Numbers:");
+            List<int> evenNumbers = ArrayHelper.FilterArray(numbers, ArrayHelper.IsEven);
+            foreach (int num in evenNumbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("\nOdd Numbers:");
+            List<int> oddNumbers = ArrayHelper.FilterArray(numbers, ArrayHelper.IsOdd);
+            foreach (int num in oddNumbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("\nPrime Numbers:");
+            List<int> primeNumbers = ArrayHelper.FilterArray(numbers, ArrayHelper.IsPrime);
+            foreach (int num in primeNumbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("\nFibonacci Numbers:");
+            List<int> fibonacciNumbers = ArrayHelper.GetFibonacciNumbers(10); 
+            foreach (int num in fibonacciNumbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
