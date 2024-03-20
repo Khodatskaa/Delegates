d0345317121a24d16f9dac97b194ab3d4e4caa68
Khodatskaa <khodatskaalina@gmail.com>
2024-03-20 19:56:55 +0200

    Ex 3 was done

---
+++ Delegates/CreditCard.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class CreditCard
+    {
+        public string CardNumber { get; private set; }
+        public string OwnerName { get; private set; }
+        public DateTime Validity { get; private set; }
+        public string PIN { get; private set; }
+        public decimal CreditLimit { get; private set; }
+        public decimal AvailableFunds { get; private set; }
+
+        public event EventHandler<decimal> RechargeEvent;
+        public event EventHandler<decimal> ExpenditureEvent;
+        public event EventHandler<decimal> CreditUtilizationEvent;
+        public event EventHandler<decimal> LimitReachedEvent;
+        public event EventHandler<string> PINChangedEvent;
+
+        public CreditCard(string cardNumber, string ownerName, DateTime validity, string pin, decimal creditLimit)
+        {
+            CardNumber = cardNumber;
+            OwnerName = ownerName;
+            Validity = validity;
+            PIN = pin;
+            CreditLimit = creditLimit;
+            AvailableFunds = 0; 
+        }
+
+        public void Recharge(decimal amount)
+        {
+            AvailableFunds += amount;
+            OnRechargeEvent(amount);
+        }
+
+        public void Spend(decimal amount)
+        {
+            if (AvailableFunds >= amount)
+            {
+                AvailableFunds -= amount;
+                OnExpenditureEvent(amount);
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds!");
+            }
+        }
+
+        public void UtilizeCredit(decimal amount)
+        {
+            if (AvailableFunds + amount <= CreditLimit)
+            {
+                AvailableFunds += amount;
+                OnCreditUtilizationEvent(amount);
+            }
+            else
+            {
+                OnLimitReachedEvent(CreditLimit - AvailableFunds);
+            }
+        }
+
+        public void ChangePIN(string newPIN)
+        {
+            PIN = newPIN;
+            OnPINChangedEvent(newPIN);
+        }
+
+        protected virtual void OnRechargeEvent(decimal amount)
+        {
+            RechargeEvent?.Invoke(this, amount);
+        }
+
+        protected virtual void OnExpenditureEvent(decimal amount)
+        {
+            ExpenditureEvent?.Invoke(this, amount);
+        }
+
+        protected virtual void OnCreditUtilizationEvent(decimal amount)
+        {
+            CreditUtilizationEvent?.Invoke(this, amount);
+        }
+
+        protected virtual void OnLimitReachedEvent(decimal exceededAmount)
+        {
+            LimitReachedEvent?.Invoke(this, exceededAmount);
+        }
+
+        protected virtual void OnPINChangedEvent(string newPIN)
+        {
+            PINChangedEvent?.Invoke(this, newPIN);
+        }
+    }
+
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -2,8 +2,45 @@
 {
     internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
+            CreditCard card = new CreditCard("1234-5678-9012-3456", "John Doe", DateTime.Now.AddYears(3), "5864", 1000);
+
+            card.RechargeEvent += RechargeEventHandler;
+            card.ExpenditureEvent += ExpenditureEventHandler;
+            card.CreditUtilizationEvent += CreditUtilizationEventHandler;
+            card.LimitReachedEvent += LimitReachedEventHandler;
+            card.PINChangedEvent += PINChangedEventHandler;
+
+            card.Recharge(500); 
+            card.Spend(200);    
+            card.UtilizeCredit(800); 
+            card.ChangePIN("9271");  
+        }
+
+        static void RechargeEventHandler(object sender, decimal amount)
+        {
+            Console.WriteLine($"Recharge of {amount:C} was successful.");
+        }
+
+        static void ExpenditureEventHandler(object sender, decimal amount)
+        {
+            Console.WriteLine($"Expenditure of {amount:C} was successful.");
+        }
+
+        static void CreditUtilizationEventHandler(object sender, decimal amount)
+        {
+            Console.WriteLine($"Credit utilization of {amount:C} was successful.");
+        }
+
+        static void LimitReachedEventHandler(object sender, decimal exceededAmount)
+        {
+            Console.WriteLine($"Limit reached. Exceeded amount: {exceededAmount:C}");
+        }
+
+        static void PINChangedEventHandler(object sender, string newPIN)
+        {
+            Console.WriteLine($"PIN changed successfully. New PIN: {newPIN}");
         }
     }
 }
